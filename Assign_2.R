79c6c28ff24a8c300c6baa97647f5b10f667c267
RMcClintock <rick_mcc@yahoo.com>
2019-04-11 10:35:21 -0400

    Second attempt

---
+++ Assign_2.R
@@ -0,0 +1,7 @@
+# Assignment 2 Coursera R Programming
+# Caching the Inverse of a Matrix
+# Two functions:    makeCacheMatrix:  A special "matrix"
+#                                     that can cache its inverse
+#                   cacheSolve: Function to compute the inverse of a
+#                               a matrix returned by makeCacheMatrix
+#
