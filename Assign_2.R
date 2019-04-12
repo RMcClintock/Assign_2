b2c6901c8b421ab67d2bace551f456b958216d74
RMcClintock <rick_mcc@yahoo.com>
2019-04-12 08:05:00 -0400

    Updated Comments and beginning cacheSolve function

--- Assign_2.R
+++ Assign_2.R
@@ -1,13 +1,15 @@
 # Assignment 2 Coursera R Programming
 # Caching the Inverse of a Matrix
-# Two functions:    makeCacheMatrix:  A special "matrix"
-#                                     that can cache its inverse
-#                   cacheSolve: Function to compute the inverse of a
-#                               a matrix returned by makeCacheMatrix
+# Two functions:    makeCacheMatrix:  A special "matrix" object
+#                                     that can cache the matrix's inverse
+#                   cacheSolve: Function to compute or retrieve the inverse 
+#                               of a matrix returned by makeCacheMatrix
 #
 makeCacheMatrix <- function(matx = matrix(),...) {
     # matx is a square matrix that is invertible
-    # returns a list containing four functions
+    #
+    # returns a list containing four functions used in calculating
+    # or retrieving the inverse of a matrix
     #
     imat <- NULL      # initialize the inverse matrix
     #
@@ -37,6 +39,22 @@ makeCacheMatrix <- function(matx = matrix(),...) {
         # This function will return the imat object from parent environment
         imat
     }    
+    #
+    # return with the four functions used to set(store) a matrix,
+    # get(retrieve) the matrix, set(store) the method to calculate the 
+    # inverse of the matrix, and get(retrieve) the inverse of the matrix.
+    #
     list(setmat = setmat, getmat = getmat,
          setsolve = setsolve, getsolve = getsolve)
+    #
 }  #  end of makeCacheMatrix
+#
+cacheSolve <- function(matx,...) {
+    # matx is a square matrix that is invertible
+    #
+    # Using the inverse matrix process in makeCacheMatrix, this function
+    # will return either compute the inverse of a matrix or retrieve 
+    # the inverse if it has already been computed.
+    #
+    
+}
